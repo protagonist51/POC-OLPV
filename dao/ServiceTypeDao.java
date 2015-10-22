47f13c0fc0004df2c9c9f6819ea56d9727ed8456
yogesh51 <yogeshyvb@gmail.com>
2015-10-22 15:08:00 +0530

    Create ServiceTypeDao.java

---
+++ dao/ServiceTypeDao.java
@@ -0,0 +1,11 @@
+package org.itc.dao;
+
+import java.util.List;
+
+import org.itc.model.ServiceType;
+
+public interface ServiceTypeDao {
+	
+	public List<ServiceType> serviceTableInformation( );
+
+}
