bc62b0989e793a1d7b6457a29bab88b152f37ecf
yogesh51 <yogeshyvb@gmail.com>
2015-10-30 18:53:29 +0530

    Update adminController.js

--- js/adminController.js
+++ js/adminController.js
@@ -1,27 +1,28 @@
-app.controller('adminController',[ '$scope', '$http', '$location',  '$rootScope' ,'$timeout',function($scope, $http, $location, $rootScope, $timeout) {
+app.controller('adminController',[ '$scope', '$http', '$location', '$rootScope', '$window', function($scope, $http, $location, $rootScope, $window) {
+		
+$rootScope.Username = $window.sessionStorage.userName;
 	
-
+	if($location.path()!= '/homepage' ){
+		
+		$rootScope.logoutHide = false;
+	}else{
+		$rootScope.logoutHide = true;
+	}
+	
 	$http.get('getAlldetails').success(function(response)
-			
-    	 	{	
+    	 	{
 				$scope.data = response;
 				
-				
-			}).error(function(error)
+				}).error(function(error)
                     {
                 alert(error);
             });
 	
-
-	 
-	 
 	
 	$scope.editUser = function(id)
 	{
-	
+		
 		$location.url('/editUser/'+ id);
 	}
-	
 
-	
 }]);
