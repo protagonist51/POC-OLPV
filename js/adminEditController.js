468623688ec97f08086018ed8d323a653c6e4a95
yogesh51 <yogeshyvb@gmail.com>
2015-10-30 18:53:50 +0530

    Update adminEditController.js

--- js/adminEditController.js
+++ js/adminEditController.js
@@ -1,72 +1,72 @@
 
 app.controller('adminEditController',[ '$scope', '$http', '$location', '$routeParams', function($scope, $http, $location, $routeParams) {
     
-	 $scope.status  = 'Waiting';
-
      $scope.options = [
-       { id : '1', name: 'Verified' }
-      ,{ id : '2', name: 'Rejected' }
-      ,{ id : '3', name: 'Waiting' }
+      { id : '1', name: 'Open' },
+      { id : '2', name: 'In-Progress' },
+      { id : '3', name: 'Rejected' },
+      { id : '4', name: 'Verified' },
+      { id : '5', name: 'Closed' }
       ];
-     
+      
 	$scope.id = $routeParams.id;
       
        $http.post('editUserdetails', $scope.id).success(function(response)
        {
               $scope.data = response;
-             
- 
+         
        }).error(function(error)
             {
         alert(error);
     });
        
-       
-       
-       $http.post('viewDoc', $scope.id).success(function(re){
-      	  
-       	 
-      	  $scope.doc = re;
-      	
-      	  
-        }).error(function(error)
-                {
-            alert(error);
-        });
-       
-       
-       
-       
+      $http.post('viewDoc', $scope.id).success(function(re){
+     	  
+     	 
+     	  $scope.doc = re;
+     	
+     	  
+       }).error(function(error)
+               {
+           alert(error);
+       });
+      
+       $scope.statusChange = function()
+       {
+    	   $scope.data.status.statusId = $scope.status;
+    	   
+    	   if($scope.data.status.statusId == 3)
+    	   {	
+    	   		
+ 			   $scope.isRejected=true;
+    	   		
+    	   		
+    	   } else {
+    		  
+    		   $scope.isRejected=false;
+    	   }
+       };
        
        $scope.submit = function(){
-           
     	  
-    	
-           var formData = {
-        		   status	:{ statusId:$scope.status, status:''},
-                        id	: $scope.id
-           };
-           $scope.data.status.statusId = $scope.status;
+           $scope.data.comment = $scope.comment;
           
-           $http.post('userdetails', $scope.data).success(function(data)
-        	      {	
-        	   
+           
+           $http.post('userdetails', $scope.data).success(function(response)
+        	      {
         	   			$location.url('/admin');
-        	   		  
         	  			
         	      }).error(function(error)
         	              {
         	          alert(error);
         	      });
-         
-          
+    	   
        };
        
-       
-       $scope.comeback = function(){
-    	   $location.url('/admin');
+       $scope.comeback = function()
+       {
     	   
+    	   $location.url('/admin');
        }
-       
       
 }]);
