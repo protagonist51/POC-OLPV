8f0bd30a25283a0b350675fadaf9fb280cdaf5d6
yogesh51 <yogeshyvb@gmail.com>
2015-10-30 18:55:42 +0530

    Update viewDetailscontroller.js

--- js/viewDetailscontroller.js
+++ js/viewDetailscontroller.js
@@ -1,35 +1,31 @@
-app.controller('viewDetailsController',[ '$scope', '$http', '$location', '$routeParams',  function($scope, $http, $location, $routeParams)
-                                          
+app.controller('viewDetailsController',[ '$scope', '$http', '$location', '$routeParams','$rootScope', '$window',  function($scope, $http, $location, $routeParams, $rootScope, $window)
     {
-		
 				$scope.userId = $routeParams.userId;
-		
-			
+				$rootScope.Username = $window.sessionStorage.userName;
+				
+				if($location.path()!= '/homepage' )
+				{
+				 $rootScope.logoutHide = false;
+				}else
+				{
+					$rootScope.logoutHide = true;
+				}
 			
 				$http.post('viewuserdetails', $scope.userId ).success(function(response)
 						{
 			
-			
 						$scope.detailsforuser = response;
-			
 			
 						}).error(function(error)
 								
 								{
 								alert(error);
 								});
-		
 			
-				
-					
-				
 						$scope.viewdetails =function(id)
 						
 						{
-				
-							
 							$location.url('/userwelcome/'+id);
-				
 						};
 						
 						
@@ -38,10 +34,7 @@ app.controller('viewDetailsController',[ '$scope', '$http', '$location', '$route
 						
 							$location.url('/details/'+$scope.userId);
 									
-							
 						}
-						
-						
 						
 						$scope.Logout = function()
 						{	
@@ -51,9 +44,6 @@ app.controller('viewDetailsController',[ '$scope', '$http', '$location', '$route
 										
 										$location.url('/homepage');
 									});
-							
 						}
-						
-						
 						
 	}]);
