c1ff95443ef634931fd777e21c7196ec373f21ce
yogesh51 <yogeshyvb@gmail.com>
2015-11-02 18:53:31 +0530

    Update adminController.js

--- js/adminController.js
+++ js/adminController.js
@@ -9,9 +9,36 @@ $rootScope.Username = $window.sessionStorage.userName;
 		$rootScope.logoutHide = true;
 	}
 	
+	 $scope.options = [
+	                   { id : '1', name: 'Open' },
+	                   { id : '2', name: 'In-Progress' },
+	                   { id : '3', name: 'Rejected' },
+	                   { id : '4', name: 'Verified' },
+	                   { id : '5', name: 'Closed' }
+	                   ];
+	
 	$http.get('getAlldetails').success(function(response)
     	 	{
 				$scope.data = response;
+				for(var i=0; i<response.length; i++)
+					{
+						
+					console.log(response.id);
+					}
+				$scope.totalItems = response.length;
+		        $scope.viewby = 10;
+		        $scope.currentPage = 1;
+		        $scope.itemsPerPage = $scope.viewby;
+		        $scope.maxSize = 5; //Number of pager buttons to show
+
+		        $scope.setPage = function (pageNo) {
+		            $scope.currentPage = pageNo;
+		        };
+
+		        $scope.setItemsPerPage = function(num) {
+		            $scope.itemsPerPage = num;
+		            $scope.currentPage = 1; 
+		            }
 				
 				}).error(function(error)
                     {
@@ -24,5 +51,22 @@ $rootScope.Username = $window.sessionStorage.userName;
 		
 		$location.url('/editUser/'+ id);
 	}
+	
+	
+	$scope.statusChange = function()
+	{
+		//$scope.status.statusId = $scope.status;
+		alert($scope.response.status.statusId);
+		var count = 0;
+		for (var i = 0 ; i < $scope.totalItems; i++){
+			if($scope.data.status.statusId ==  $scope.status){
+			count ++;
+			}
+		}
+	$scope.totalItems = count;
+	
+	}
 
 }]);
+
+
