901301aa5adcbe8a68279e6f70d58312f988c195
yogesh51 <yogeshyvb@gmail.com>
2015-11-06 18:37:48 +0530

    Update detailsController.js

--- js/detailsController.js
+++ js/detailsController.js
@@ -1,5 +1,5 @@
 
-app.controller('detailsController',[ '$scope', '$http', '$location', '$routeParams', '$filter', '$upload','$window', '$rootScope', function($scope, $http, $location, $routeParams, $upload, $filter,$window, $rootScope)
+app.controller('detailsController',[ '$scope', '$http', '$location', '$routeParams', '$filter', '$upload','$window', '$rootScope', '$upload', function($scope, $http, $location, $routeParams, $upload, $filter, $window, $rootScope, $upload)
   {
  
        $scope.userId = $routeParams.userId;
@@ -58,21 +58,21 @@ app.controller('detailsController',[ '$scope', '$http', '$location', '$routePara
       
          
          
-         $scope.submitForm = function(isValid) {											/*form validation for details and showing modal for confirm details*/
-				
+         $scope.submitForm = function(isValid) {
+        	 /*form validation for details and showing modal for confirm details*/
+			if (isValid){	
 				if	( $scope.items.length < 2)
 				{
 					$('#myModalErr1').modal('show');
 					return false;
 				}
 
-			  if (isValid) {
 			    	$('#saveDetails').modal('show');
-			    }
+			
 			
 			 }
          
-       
+         }
          $scope.nationality='';
          $scope.countries = ['Afghanistan', 'Australia', 'Bangladesh', 'Brazil', 'Britan', 'Canada', 'China', 'Denmark', 'Egypt', 'France', 'Germany', 'India', 'Indonesia', 'Iran', 'Iraq', 'Isrel', 'Italy', 'Japan', 'Nepal', 'New Zealand', 'Pakistan', 'Sri Lanka', 'Poland', 'United States', 'South Africa', 'Russia', 'Sweeden', 'Chilie', 'Spain', 'Norway', 'Mexico', 'South Korea', 'Netherland','Irelend'];
           
@@ -103,11 +103,15 @@ app.controller('detailsController',[ '$scope', '$http', '$location', '$routePara
 			              {
 			                for (var i = 0 ; i < $scope.items.length ; i++)
 			                {
-			                  $scope.uploadItem(response.id, $scope.items[i]);
+			                  $scope.uploadItem($scope.items[i]);
 			                  
 			                }
 			              }  
-			      
+							
+//						$scope.item = $scope.items[0];
+//						
+//						$scope.uploadItem(response.id, $scope.item);
+//			      
 			             							
 						if (response.id != null)
 							
@@ -115,7 +119,7 @@ app.controller('detailsController',[ '$scope', '$http', '$location', '$routePara
 							
 							$scope.viewuser = response;
 							
-							$location.url('/userwelcome/' + response.id);
+						//	$location.url('/userwelcome/' + response.id);
 						//	$('#myModal').modal('show');
 							
 							$scope.Logout = function ()
@@ -138,15 +142,53 @@ app.controller('detailsController',[ '$scope', '$http', '$location', '$routePara
 							});
 	   
 	};
+	
+	$scope.uploadItem = function(file){
+        var fd = new FormData();
+        fd.append('file', file);
+        $http.post('upload', fd, {
+            transformRequest: angular.identity,
+            headers: {'Content-Type': ''}
+        })
+        .success(function(resp){
+        	console.log('Success ' + resp.config.data.file.name + 'uploaded. Response: ' + resp.data);
+        })
+        .error(function(resp){
+        	 console.log('Error status: ' + resp.status);
+        });
+    }
+	
+/*	$scope.uploadItem = function (file) {
+		var myfile = new Blob();
+		myfile = file;
+        $upload.upload({
+            url: 'upload',
+        	file: myfile
+        }).then(function (resp) {
+            console.log('Success ' + resp.config.data.file.name + 'uploaded. Response: ' + resp.data);
+        }, function (resp) {
+            console.log('Error status: ' + resp.status);
+        });
+	};*/
          
-         $scope.uploadItem = function(id, file)
+      /*   $scope.uploadItem = function(id, file)
          {
+        	 
+        	 $scope.upload = function (file) {
+        	        Upload.upload({
+        	            url: 'upload/url',
+        	            data: {file: file, 'id': id}
+        	        }).then(function (resp) {
+        	            console.log('Success ' + resp.config.data.file.name + 'uploaded. Response: ' + resp.data);
+        	        }, function (resp) {
+        	            console.log('Error status: ' + resp.status);
+        	        });
         	 
         	 		var data = new FormData();
         	 		data.append('id', id);
         	 		data.append('file', file);
         	 		
-        	 		/*$http({
+        	 		$http({
         	 				method: 'POST',
         	 				url  :   'multipleSave',
         	 				headers: {
@@ -154,28 +196,27 @@ app.controller('detailsController',[ '$scope', '$http', '$location', '$routePara
         	 	            },
         	 			
         	 	           headers: {'Content-Type': undefined},
-        	 	        data: data*/
+        	 	        data: data
   	 			
         	 				
         	 		    $http.post('multipleSave', data, {
         	 	        transformRequest: function(data, headersGetterFunction) {
         	 	            return data;
         	 	        },
-        	 	        headers: { 'Content-Type': undefined
-        	 	        /*enctype:'multipart/form-data'*/ }
+        	 	       headers: {'Content-Type': undefined}
   
         	 		 }).success(function(data) {
-        	            $log.debug("Upload Successfull");
-        	            $log.debug("File upload: Success calling ");
+        	            console.log("Upload Successfull");
+        	           
         	            alert(data);
-        	            alert("hi");
+        	           
         	 		}).error(function(error) {
-        	             $log.debug("Upload failure");
+        	 			console.log("Upload failure");
         	             
         	             
         	         });
          };
-        
+        */
              
     
  }]);
