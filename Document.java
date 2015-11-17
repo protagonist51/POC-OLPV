01e56d27fb748352eba8cbb218be0c79a9a46223
yogesh51 <yogeshyvb@gmail.com>
2015-11-17 19:42:41 +0530

    Update Document.java

--- Document.java
+++ Document.java
@@ -11,19 +11,13 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 
-
 @Entity
 @Table(name="documents")
 public class Document {
 
-                
                 private  int docId;
                 private  String  docPath;
                 int id;
-                private String fileSize;
-                private String fileName;
-                private String fileType;
-                private byte[] bytes;
                  
                 @Id
                 @Column(name="docId")
@@ -54,55 +48,17 @@ public class Document {
 					this.id = id;
 				}
 				
-				
-				@Column(name="fileSize")
-				public String getFileSize() {
-					return fileSize;
-				}
-
-				public void setFileSize(String fileSize) {
-					this.fileSize = fileSize;
-				}
-
-				@Column(name="fileName")
-				public String getFileName() {
-					return fileName;
-				}
-
-				public void setFileName(String fileName) {
-					this.fileName = fileName;
-				}
-
-				@Column(name="fileType")
-				public String getFileType() {
-					return fileType;
-				}
-
-				public void setFileType(String fileType) {
-					this.fileType = fileType;
-				}
-
-				@Column(name="bytes")
-				public byte[] getBytes() {
-					return bytes;
-				}
-
-				public void setBytes(byte[] bytes) {
-					this.bytes = bytes;
-				}
-
+			
 				public Document(){ 
                 	
                 }
                 
-                public Document(int docId, String docPath, int id, String fileType, String fileName, String fileSize) {
+                public Document(int docId, String docPath, int id) {
                                 super();
                                 this.docId = docId;
                                 this.docPath = docPath;
                                 this.id = id;
-                                this.fileName=fileName;
-                                this.fileSize=fileSize;
-                                this.fileType=fileType;
+                                
                 }
                 
 }
