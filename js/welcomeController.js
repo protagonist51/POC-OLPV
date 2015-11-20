d679a6e3572a928ebf633b05a4d9ec19929abb9f
yogesh51 <yogeshyvb@gmail.com>
2015-11-20 19:28:10 +0530

    Update welcomeController.js

--- js/welcomeController.js
+++ js/welcomeController.js
@@ -1,27 +1,42 @@
-app.controller('welcomeController',[ '$scope', '$http', '$location', '$routeParams',  function($scope, $http, $location, $routeParams)
+app.controller('welcomeController',[ '$scope', '$http', '$location', '$routeParams', '$window', '$route',  function($scope, $http, $location, $routeParams, $window, $route)
                                      
  {
-		
 		$scope.id = $routeParams.id;
-		$http.post('recordforid', $scope.id ).success(function(response)
-		{
-			
-			$scope.res=response;
-			
-			$http.post('viewDoc', $scope.id).success(function(re){
-		     	  
-		     	 
-		     	  $scope.doc = re;
-		     	
-		     	  
-		       }).error(function(error)
-		               {
-		           alert(error);
-		       });
-		      
-			
-		});
+		var check=false;
+		setTimeout(function(){
+			$http.post('recordforid', $scope.id ).success(function(response)
+					{
+						
+						$scope.res=response;
+						
+						$http.post('viewDoc', $scope.id).success(function(re){
+					     	  
+					     	  $scope.doc = re;
+					     	
+					     	
+					     //	alert("inside"+ check);
+					     	 // alert("Inside"+check);
+					       }).error(function(error)
+					               {
+					           console.log(error);
+					       });
+						
+					});
+     	}, 2000);
 		
+		//$route.reload();
+		
+		//sleep(1000);
+		// alert(check);
+		
+		/*if(check==true)
+			{
+			alert("inside");
+			setTimeout(function(){
+				alert("ok");
+	     		$window.location.reload();
+	     	}, 2000);
+			}*/
 	
 		  $scope.Back = function(){
 			
@@ -29,7 +44,5 @@ app.controller('welcomeController',[ '$scope', '$http', '$location', '$routePara
 	    	   
 	       }
 		
-		
-		
-		
+	
 }]);
