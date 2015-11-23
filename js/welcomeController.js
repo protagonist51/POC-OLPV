0214ad06de39ef3ce568ec39f902214c3a21f3a4
yogesh51 <yogeshyvb@gmail.com>
2015-11-23 16:25:19 +0530

    Update welcomeController.js

--- js/welcomeController.js
+++ js/welcomeController.js
@@ -1,48 +1,32 @@
-app.controller('welcomeController',[ '$scope', '$http', '$location', '$routeParams', '$window', '$route',  function($scope, $http, $location, $routeParams, $window, $route)
+app.controller('welcomeController',[ '$scope', '$http', '$location', '$routeParams', '$window', function($scope, $http, $location, $routeParams, $window)
                                      
  {
 		$scope.id = $routeParams.id;
-		var check=false;
+		$scope.num=false;
 		setTimeout(function(){
 			$http.post('recordforid', $scope.id ).success(function(response)
 					{
-						
+						$scope.num=true;
 						$scope.res=response;
-						
 						$http.post('viewDoc', $scope.id).success(function(re){
 					     	  
 					     	  $scope.doc = re;
-					     	
-					     	
-					     //	alert("inside"+ check);
-					     	 // alert("Inside"+check);
+					     	  
 					       }).error(function(error)
 					               {
 					           console.log(error);
 					       });
 						
 					});
-     	}, 2000);
+     	}, 4000);
 		
-		//$route.reload();
-		
-		//sleep(1000);
-		// alert(check);
-		
-		/*if(check==true)
+		if($scope.num==false)
 			{
-			alert("inside");
-			setTimeout(function(){
-				alert("ok");
-	     		$window.location.reload();
-	     	}, 2000);
-			}*/
-	
-		  $scope.Back = function(){
 			
-	    	   $location.url('/showdetailsforuser/' + $scope.res.userId );
-	    	   
-	       }
+				location.reload();
+			}
 		
-	
+		  $scope.Back = function(){
+	    	   $location.url('/showdetailsforuser/' + $scope.res.userId );
+	       }
 }]);
