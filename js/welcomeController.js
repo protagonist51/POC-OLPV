6d059a2334cf0f177a87e701640f463eb6ee54b3
yogesh51 <yogeshyvb@gmail.com>
2015-11-30 16:00:57 +0530

    Update welcomeController.js

--- js/welcomeController.js
+++ js/welcomeController.js
@@ -2,31 +2,25 @@ app.controller('welcomeController',[ '$scope', '$http', '$location', '$routePara
                                      
  {
 		$scope.id = $routeParams.id;
-		$scope.num=false;
-		setTimeout(function(){
+		
 			$http.post('recordforid', $scope.id ).success(function(response)
 					{
-						$scope.num=true;
 						$scope.res=response;
+						setTimeout(function(){
 						$http.post('viewDoc', $scope.id).success(function(re){
-					     	  
-					     	  $scope.doc = re;
-					     	  
-					       }).error(function(error)
-					               {
-					           console.log(error);
-					       });
+							
+					    	 $scope.doc = re;
+					    	 
+					    
+					      }).error(function(error)
+					              {
+					          console.log(error);
+					      });
+						}, 2000);
 						
 					});
-     	}, 4000);
-		
-		if($scope.num==false)
-			{
-			
-				location.reload();
-			}
-		
-		  $scope.Back = function(){
-	    	   $location.url('/showdetailsforuser/' + $scope.res.userId );
-	       }
+     	
+		 $scope.Back = function(){
+	   	   $location.url('/showdetailsforuser/' + $scope.res.userId );
+	      }
 }]);
